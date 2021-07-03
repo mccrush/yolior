fa620fca5b3e6f2d996cedf092d6edf7db2b842d
mccrush <mccrush2027@gmail.com>
2021-07-03 22:05:54 +0700

    Settings navigation view

--- src/components/index/StepsBlock.vue
+++ src/components/index/StepsBlock.vue
@@ -1,5 +1,106 @@
 <template>
-  <div class="d-flex justify-content-between mt-2">
+  <div class="row">
+    <div class="col-12 col-sm-6 col-md-3">
+      <button
+        class="
+          btn
+          shadow-sm
+          d-flex
+          justify-content-center
+          align-items-center
+          w-100
+          p-0
+        "
+        @click="showKafes"
+        :class="{
+          active: type === 'kafeId' && !showBasket
+        }"
+      >
+        <h5 class="m-0 pt-2 pb-2">Выберите кафе</h5>
+      </button>
+    </div>
+    <div class="col-12 col-sm-6 col-md-3 mt-2 mt-sm-0">
+      <button
+        class="
+          btn
+          shadow-sm
+          d-flex
+          justify-content-center
+          align-items-center
+          w-100
+          p-0
+        "
+        @click="showCategorys"
+        :class="{
+          active: type === 'categoryId' && !showBasket
+        }"
+        :disabled="!kafeId"
+      >
+        <h2 class="me-2">&#8250;</h2>
+        <h5 class="m-0 pt-2 pb-2">Категорию</h5>
+      </button>
+    </div>
+    <div class="col-12 col-sm-6 col-md-3 mt-2 mt-md-0">
+      <button
+        class="
+          btn
+          shadow-sm
+          d-flex
+          justify-content-center
+          align-items-center
+          w-100
+          p-0
+        "
+        @click="showProducts"
+        :class="{
+          active: type === 'productId' && !showBasket
+        }"
+        :disabled="!categoryId"
+      >
+        <h2 class="me-2">&#8250;</h2>
+        <h5 class="m-0 pt-2 pb-2">Блюдо</h5>
+      </button>
+    </div>
+    <div class="col-12 col-sm-6 col-md-3 mt-2 mt-md-0">
+      <button
+        class="
+          btn
+          shadow-sm
+          d-flex
+          justify-content-center
+          align-items-center
+          w-100
+          p-0
+          position-relative
+        "
+        @click="setShowBasket"
+        :class="{
+          active: showBasket
+        }"
+        :disabled="!basket.length"
+      >
+        <h2 class="me-2">&#8250;</h2>
+        <h5 class="m-0 pt-2 pb-2">Сделайте заказ</h5>
+        <span
+          v-if="basket.length"
+          class="
+            position-absolute
+            top-0
+            start-100
+            translate-middle
+            badge
+            rounded-pill
+            bg-warning
+            text-white
+          "
+        >
+          {{ basket.length }}
+          <span class="visually-hidden">Количество выбранных блюд</span>
+        </span>
+      </button>
+    </div>
+  </div>
+  <!-- <div class="d-flex justify-content-between mt-2">
     <button
       class="btn shadow-sm p-0"
       @click="showKafes"
@@ -58,7 +159,7 @@
         <span class="visually-hidden">Количество выбранных блюд</span>
       </span>
     </button>
-  </div>
+  </div> -->
 </template>
 
 <script>
