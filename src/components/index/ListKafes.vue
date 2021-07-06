6b33819d33431b5e0b0c9f54c26ad1818b2a4b71
mccrush <mccrush2027@gmail.com>
2021-07-07 03:02:21 +0700

    Add icons for product card

--- src/components/index/ListKafes.vue
+++ src/components/index/ListKafes.vue
@@ -54,6 +54,25 @@
           @click="addToBasket(item)"
         >
           В корзину
+          <!-- <img
+            src="img/work_icons/cart-plus.svg"
+            width="24"
+            height="24"
+            alt="Добавить в корзину"
+            class="opacity-06"
+          /> -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            class="bi bi-cart-plus-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"
+            />
+          </svg>
         </button>
       </div>
     </div>
@@ -91,5 +110,9 @@ export default {
 .stretched-link {
   color: #212529;
   text-decoration: none;
+}
+
+.opacity-06 {
+  opacity: 0.6;
 }
 </style>
