cac4e05cb7fda752abcd53c542ae39cf2ad67fa7
mccrush <mccrush2027@gmail.com>
2021-07-19 15:17:09 +0700

    Settings work SW

--- public/run-sw.js
+++ public/run-sw.js
@@ -2,7 +2,7 @@ window.addEventListener('load', async () => {
   if (navigator.serviceWorker) {
     try {
       const reg = await navigator.serviceWorker.register('/sw.js')
-      console.log('SW success register:', reg)
+      console.log('SW success register')
     } catch (error) {
       console.log('SW rerister with error:', error)
     }
--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,44 @@
-self.addEventListener('install', event => {
-  console.log('[SW]: install');
+const staticCacheName = 'svy-v4'
+
+const assetUrls = [
+  '/index.html',
+  '/js/app.js',
+  '/js/chunk-vendors.js',
+  '/sw.js',
+  '/run-sw.js',
+  '/manifest.json',
+  '/img/food.png',
+  '/icons/favicon-32x32.png',
+  '/icons/android-chrome-192x192.png',
+  '/logo.svg',
+  '/img/index/steps/1.svg',
+  '/img/index/steps/2.svg',
+  '/img/index/steps/3.svg',
+  '/img/index/steps/4.svg'
+]
+
+self.addEventListener('install', async event => {
+  console.log('[SW]: install')
+  const cache = await caches.open(staticCacheName)
+  await cache.addAll(assetUrls)
 })
 
-self.addEventListener('activate', event => {
-  console.log('[SW]: activate');
+self.addEventListener('activate', async event => {
+  console.log('[SW]: activate')
+  const cachesNames = await caches.keys()
+  await Promise.all(
+    cachesNames
+      .filter(name => name !== staticCacheName)
+      .map(name => caches.delete(name))
+  )
 })
+
+self.addEventListener('fetch', event => {
+  console.log('[SW]: fetch')
+  event.respondWith(cacheFirst(event.request))
+})
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request)
+  return cached ?? await fetch(request)
+}
