b1033afefc365ca0081506ef92a8958ebd7aeda1
mccrush <mccrush2027@gmail.com>
2021-08-03 10:30:43 +0700

    Add modal for card

--- src/components/index/ListKafes.vue
+++ src/components/index/ListKafes.vue
@@ -43,72 +43,25 @@
           </div>
         </div>
         <div v-if="type === 'productId'">
-          <button
-            v-if="basket.find(el => el.id === item.id)"
-            class="
-              btn btn-success
-              rounded-0 rounded-bottom
-              text-white
-              w-100
-              pt-3
-              pb-3
-            "
-            @click="removeProductFromBasket(item.id)"
-          >
-            Добавлено
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="currentColor"
-              class="bi bi-cart-check"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z"
-              />
-              <path
-                d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
-              />
-            </svg>
-          </button>
-
-          <button
-            v-else
-            class="
-              btn btn-warning
-              rounded-0 rounded-bottom
-              text-white
-              w-100
-              pt-3
-              pb-3
-            "
-            @click="addToBasket(item)"
-          >
-            В корзину
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              fill="currentColor"
-              class="bi bi-cart-plus-fill"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"
-              />
-            </svg>
-          </button>
+          <ButtonAddToBasket :item="item" />
         </div>
       </div>
     </div>
+    <ModalCard v-if="item" id="cardModal" :item="item" />
   </div>
 </template>
 
 <script>
+import Modal from 'bootstrap/js/dist/modal'
+import ModalCard from '@/components/index/ModalCard'
+import ButtonAddToBasket from '@/components/index/ButtonAddToBasket'
 import sortMethod from '@/scripts/sortMethod'
 
 export default {
+  components: {
+    ModalCard,
+    ButtonAddToBasket
+  },
   props: {
     list: Array,
     type: String
@@ -116,20 +69,18 @@ export default {
   computed: {
     sortList() {
       return sortMethod(this.list, 'asc', 'position')
-    },
-    basket() {
-      return this.$store.getters.basket
     }
   },
   methods: {
     setItemId(id) {
       this.$store.commit('setValue', { type: this.type, id })
+      if (this.type === 'productId') {
+        this.showModal()
+      }
     },
-    addToBasket(item) {
-      this.$store.commit('addToBasket', item)
-    },
-    removeProductFromBasket(id) {
-      this.$store.commit('removeProductFromBasket', id)
+    showModal() {
+      const myModal = new Modal(document.getElementById('cardModal'))
+      myModal.show()
     }
   }
 }
---
+++ src/components/index/ModalCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div
+    class="modal fade"
+    tabindex="-1"
+    aria-labelledby="cardModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-fullscreen-sm-down">
+      <div class="modal-content border-0">
+        <div class="modal-header">
+          <!-- <h5 class="modal-title" id="exampleModalLabel">О проекте</h5> -->
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <img
+            v-if="item.image"
+            :src="item.image"
+            class="card-img-top"
+            :alt="item.title"
+          />
+          <img
+            v-else
+            src="/img/index/noimage.jpg"
+            class="card-img-top"
+            :alt="item.title"
+          />
+          <h6 class="d-sm-none card-title lh-1 mb-0" id="cardModalLabel">
+            {{ item.title }}
+          </h6>
+          <h5 class="d-none d-sm-block card-title lh-1 mb-0">
+            {{ item.title }}
+          </h5>
+          <div v-if="type === 'productId'" class="card-text">
+            <div class="lh-1 small mt-2 mb-2">{{ item.description }}</div>
+            <span v-if="item.weight" class="badge bg-light text-dark me-2 p-1">
+              {{ item.weight }} г</span
+            >
+            <span v-if="item.amount" class="badge bg-light text-dark me-2 p-1">
+              {{ item.amount }} шт</span
+            >
+            <span v-if="item.volume" class="badge bg-light text-dark me-2 p-1">
+              {{ item.volume }} л</span
+            >
+            <span class="badge bg-white text-success border border-success p-1">
+              {{ item.price }} ₽</span
+            >
+          </div>
+          <div class="modal-footer">
+            <ButtonAddToBasket :item="item" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonAddToBasket from '@/components/index/ButtonAddToBasket'
+
+export default {
+  components: {
+    ButtonAddToBasket
+  },
+  props: {
+    item: Object
+  }
+}
+</script>
